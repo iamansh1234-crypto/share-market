12f5ef3cf07fe53e4c7343cc640da602da48bc90
iamansh1234-crypto <singhanshrajput5@gmail.com>
2025-10-27 23:23:52 +0530

    fix: update cron schedule for daily news summary to run at noon

--- lib/inngest/function.ts
+++ lib/inngest/function.ts
@@ -72,7 +72,7 @@ export const sendSignUpEmail = inngest.createFunction(
 
 export const sendDailyNewsSummary = inngest.createFunction(
     { id: 'daily-news-summary' },
-    [ { event: 'app/send.daily.news' }, { cron: '* 10 * * *' } ],
+    [ { event: 'app/send.daily.news' }, { cron: '* 12 * * *' } ],
     async ({ step }) => {
         // Step #1: Get all users for news delivery
         const users = await step.run('get-all-users', getAllUsersForNewsEmail)
